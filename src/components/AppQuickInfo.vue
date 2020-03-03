3ce5f0c11435cae41b8fa6b782c023ea8d618764
longo-andrea <longoandrea.dev@gmail.com>
2020-03-03 21:55:24 +0100

    Added tabs in homepage, #6

---
+++ src/components/AppQuickInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-tabs
+      v-model="activeTab"
+      type="border-card"
+      :stretch="true">
+      <el-tab-pane
+        label="Working on"
+        name="working-on">Working on</el-tab-pane>
+      <el-tab-pane
+        label="Deadlines"
+        name="deadlines">Deadlines</el-tab-pane>
+  </el-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppQuickInfo',
+  data() {
+    return {
+      activeTab: 'working-on',
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="home">
+  <div>
+    <app-quick-info></app-quick-info>
   </div>
 </template>
 
 <script>
+import AppQuickInfo from '../components/AppQuickInfo.vue';
+
 export default {
   name: 'Home',
   components: {
+    AppQuickInfo,
   },
 };
 </script>
