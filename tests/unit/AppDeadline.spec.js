a4bf03ad5f9f8d908870119b3e0b8d688bc60990
longo-andrea <longoandrea.dev@gmail.com>
2020-03-06 21:40:08 +0100

    Added AppDeadline's component unit tests

---
+++ tests/unit/AppDeadline.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount } from '@vue/test-utils';
+import { expect } from 'chai';
+import AppDeadline from '../../src/components/AppDeadline.vue';
+
+describe('AppDeadline', () => {
+  
+  it('has project name', () => {
+    const projectName = 'Test Project';
+    const wrapper = shallowMount(AppDeadline, {
+      propsData: {
+        project: projectName,
+      },
+    });
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.find('.deadline-title').text()).to.equals(projectName);
+  });
+
+  it('has deadline date', () => {
+    const deadlineDate = '2020-03-05';
+    const wrapper = shallowMount(AppDeadline, {
+      propsData: {
+        date: deadlineDate,
+      },
+    });
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.find('.deadline-date').text()).to.equals(deadlineDate);
+  });
+});
