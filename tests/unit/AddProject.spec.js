cec2d69d23995f5955c759616045091b41f77b71
longo-andrea <longoandrea.dev@gmail.com>
2020-03-13 22:39:48 +0100

    Added Add New Project component unit test, close #10

---
+++ tests/unit/AddProject.spec.js
@@ -0,0 +1,72 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { expect } from 'chai';
+import Vuex from 'vuex';
+import AddProject from '../../src/views/AddProject.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('AddProject', () => {
+  it('isLastDeadlineDateDefine return false when there are one deadline but date isn\'t defined yet', () => {
+    const wrapper = shallowMount(AddProject);
+    wrapper.vm.$data.addProjectForm.deadlinesDate.push({
+      date: '',
+    });
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.vm.isLastDeadlinesDateDefined).to.be.false;
+  });
+  it('isDeadlineDateFull return true when reach the deadlines limit', () => {
+    const wrapper = shallowMount(AddProject);
+    wrapper.vm.$data.addProjectForm.deadlinesDate.push(
+      {},
+      {},
+      {},
+      {},
+    );
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.vm.isDeadlinesDateFull).to.be.true;
+  });
+  it('isDeadlineDateEmpty return true when there are no deadlines', () => {
+    const wrapper = shallowMount(AddProject);
+    wrapper.vm.$data.addProjectForm.deadlinesDate.push();
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.vm.isDeadlinesDateEmpty).to.be.true;
+  });
+  it('is deadline added', () => {
+    const wrapper = shallowMount(AddProject);
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(1);
+    wrapper.vm.addDeadline();
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(2);
+  });
+  it('deadline was not added if the limit was reached', () => {
+    const wrapper = shallowMount(AddProject);
+    wrapper.vm.$data.addProjectForm.deadlinesDate.push(
+      {},
+      {},
+      {},
+      {},
+    );
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(5);
+    wrapper.vm.addDeadline();
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(5);
+  });
+  it('is deadline removed', () => {
+    const wrapper = shallowMount(AddProject);
+    wrapper.vm.$data.addProjectForm.deadlinesDate.push(
+      {},
+    );
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(2);
+    wrapper.vm.removeDeadline();
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(1);
+  });
+  it('isn\'t deadline removed if there is only one', () => {
+    const wrapper = shallowMount(AddProject);
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(1);
+    wrapper.vm.removeDeadline();
+    /* eslint-disable no-unused-expressions */
+    expect(wrapper.vm.$data.addProjectForm.deadlinesDate.length).to.equal(1);
+  });
+});
