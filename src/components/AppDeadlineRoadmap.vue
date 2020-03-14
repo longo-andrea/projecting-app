582e98931a8f63da4aaa49a85480f3f8cd62d8a4
longo-andrea <longoandrea.dev@gmail.com>
2020-03-15 00:26:42 +0100

    Added add task on project deadline, #11

--- src/components/AppDeadlineRoadmap.vue
+++ src/components/AppDeadlineRoadmap.vue
@@ -10,6 +10,22 @@
         <app-deadline-task
           v-bind:projectId="projectId"
           v-bind:tasks="getDeadlineTasks(tasks, deadline.id)" />
+        <el-form
+          :inline="true"
+          :model="addTaskForm"
+          :rules="rules"
+          :hide-required-asterisk="true"
+          ref="addTaskForm">
+          <el-form-item label="Task name" prop="taskName">
+            <el-input v-model="addTaskForm.taskName" placeholder="Type task name" />
+          </el-form-item>
+          <el-form-item label="Task description" prop="taskDescription">
+            <el-input type="textarea" v-model="addTaskForm.taskDescription" placeholder="Type task description" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('addTaskForm')">Query</el-button>
+          </el-form-item>
+        </el-form>
       </el-collapse-item>
     </el-collapse>
   </div>
@@ -40,10 +56,37 @@ export default {
   data() {
     return {
       activeDeadline: [0],
+      addTaskForm: {
+        taskName: '',
+        taskDescription: '',
+      },
+      rules: {
+        taskName: [
+          { required: true, message: 'Please insert task\'s name', trigger: 'blur' },
+        ],
+        taskDescription: [
+          { required: true, message: 'Please insert task\'s description', trigger: 'blur' },
+        ],
+      },
     };
   },
   methods: {
     getDeadlineTasks: (tasks, index) => tasks.filter((task) => task.deadlineIndex === index),
+    submitAddTask(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // store the results of form
+          this.$store.commit('addTask', {
+            // project id
+            // deadline id
+            taskName: this.addTaskForm.taskName,
+            taskDescription: this.addTaskForm.taskDescription,
+          });
+          // reset the form
+          this.$refs[formName].resetFields();
+        }
+      });
+    },
   },
 };
 </script>
