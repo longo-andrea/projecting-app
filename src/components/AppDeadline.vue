d9d16895627f80a124de2fe5d14404af2f8a79fc
longo-andrea <longoandrea.dev@gmail.com>
2020-03-22 14:41:40 +0100

    Added deadline's expiration check, #20

--- src/components/AppDeadline.vue
+++ src/components/AppDeadline.vue
@@ -8,7 +8,12 @@
         <p class="description">{{ deadlineDate }}</p>
       </el-col>
       <el-col :span="8">
-        <h2 class="subtitle" :class="{ 'incoming-deadline-warning': incomingDeadline }">{{ daysToDeadline }}</h2>
+        <h2 class="subtitle"
+          :class="{ 'incoming-deadline-warning': incomingDeadline,
+                  'incoming-deadline-danger': expiredDeadline }"
+        >
+          {{ daysToDeadline }}
+        </h2>
       </el-col>
     </el-row>
   </div>
@@ -33,25 +38,63 @@ export default {
       const year = date.getFullYear();
       const month = date.getMonth();
       const day = date.getDate();
+
       return `${year}/${month + 1}/${day}`;
     },
     daysToDeadline() {
       const deadlineDate = new Date(this.date);
       const currentDate = new Date(Date.now());
-      const timeLeft = Math.abs(deadlineDate - currentDate);
+      let timeLeft = deadlineDate - currentDate;
+
+      // if deadline date isn't still expired
+      if (timeLeft > 0) {
+        timeLeft = Math.abs(timeLeft);
+        const daysLeft = Math.ceil(timeLeft / (1000 * 60 * 60 * 24));
+
+        return daysLeft > 1 ? `${daysLeft} days left` : `${daysLeft} day left`;
+      }
+
+      // if deadline date is expired
+      timeLeft = Math.abs(timeLeft);
       const daysLeft = Math.ceil(timeLeft / (1000 * 60 * 60 * 24));
-      return daysLeft > 1 ? `${daysLeft} days left` : `${daysLeft} day left`;
+
+      return daysLeft > 1 ? `${daysLeft} days ago` : `${daysLeft} day ago`;
     },
     incomingDeadline() {
       const deadlineDate = new Date(this.date);
       const currentDate = new Date(Date.now());
-      const timeLeft = Math.abs(deadlineDate - currentDate);
-      const daysLeft = Math.ceil(timeLeft / (1000 * 60 * 60 * 24));
-      if (daysLeft <= 3) {
-        return true;
+      const timeLeft = deadlineDate - currentDate;
+
+      // check if deadline isn't still expired
+      if (timeLeft > 0) {
+        const daysLeft = Math.ceil(timeLeft / (1000 * 60 * 60 * 24));
+
+        // if deadline expiration is less than 3 days
+        if (daysLeft <= 3) {
+          return true;
+        }
+
+        // if deadline expiration is more than 3 days
+        return false;
       }
+
+      // if deadline is expired
       return false;
     },
+    expiredDeadline() {
+      const deadlineDate = new Date(this.date);
+      const currentDate = new Date(Date.now());
+      const timeLeft = deadlineDate - currentDate;
+
+      // if deadline is expired
+      if (timeLeft < 0) {
+        return true;
+      }
+
+      // if deadline isn't still expired
+      return false;
+    },
+
   },
 };
 </script>
@@ -66,6 +109,10 @@ export default {
 }
 
 .incoming-deadline-warning {
+  color: $--warning;
+}
+
+.incoming-deadline-danger {
   color: $--danger;
 }
 </style>
