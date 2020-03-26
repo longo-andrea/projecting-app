0e283e82b634371120c7d3f5bc3c1bda616719c8
longo-andrea <longoandrea.dev@gmail.com>
2020-03-27 00:13:48 +0100

    Added edit project feature, close #23

--- src/components/AppProjectHeader.vue
+++ src/components/AppProjectHeader.vue
@@ -18,9 +18,37 @@
       </el-col>
     </el-row>
     <div class="buttons-edit">
-      <i class="el-icon-edit button-edit" />
+      <i class="el-icon-edit button-edit" @click="isEditProjectDialogVisible = true" />
       <i class="el-icon-delete button-delete" @click="deleteProject()" />
     </div>
+    <el-dialog
+      title="Edit Project"
+      :visible.sync="isEditProjectDialogVisible"
+      width="80%">
+      <el-form
+        :model="editProjectForm"
+        :rules="rules"
+        :hide-required-asterisk="true"
+        ref="editProjectForm">
+        <el-form-item label="Project name" prop="projectName">
+          <el-input v-model="editProjectForm.projectName" placeholder="Type project name" />
+        </el-form-item>
+        <el-form-item label="Project description" prop="projectDescription">
+          <el-input
+            type="textarea"
+            v-model="editProjectForm.projectDescription"
+            placeholder="Type project description" />
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="isEditProjectDialogVisible = false">Cancel</el-button>
+        <el-button
+          @click="submitEditProject('editProjectForm')"
+          type="primary">
+          Edit Task
+        </el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -41,6 +69,23 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      isEditProjectDialogVisible: false,
+      editProjectForm: {
+        projectName: this.projectName,
+        projectDescription: this.projectInfo,
+      },
+      rules: {
+        projectName: [
+          { required: true, message: 'Please insert project\'s name', trigger: 'blur' },
+        ],
+        projectDescription: [
+          { required: true, message: 'Please insert project\'s description', trigger: 'blur' },
+        ],
+      },
+    };
+  },
   computed: {
     isProjectCompleted() {
       return this.$store.getters.getProjects
@@ -59,6 +104,20 @@ export default {
     deleteProject() {
       this.$router.replace('/');
       this.$store.commit('deleteProject', { projectId: this.projectId });
+    },
+    submitEditProject(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // store the results of form
+          this.$store.commit('editProject', {
+            projectId: this.projectId,
+            projectName: this.editProjectForm.projectName,
+            projectDescription: this.editProjectForm.projectDescription,
+          });
+          // reset the form
+          this.isEditProjectDialogVisible = false;
+        }
+      });
     },
   },
 };
