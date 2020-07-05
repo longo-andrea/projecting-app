3a54e296ed25e47f2975551d8e74c4308e15c564
Andrea Longo <longoandrea.dev@gmail.com>
2020-07-05 22:30:12 +0200

    Added card and box components

---
+++ src/components/PBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="box">
+    <div class="box__header">
+      <slot name="header" />
+    </div>
+    <div class="box__content">
+      <slot name="content" class="box__content" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PBox',
+};
+</script>
+
+<style lang="scss">
+.box {
+  width: 100%;
+  padding: 1rem .5rem;
+
+  border: $base-border;
+  border-radius: $base-border-radius;
+
+  .box__header {
+    margin: .5rem 0;
+    padding-bottom: 1rem;
+
+    border-bottom: $darken-border;
+    font-weight: $font-bold;
+    font-size: 1.5rem;
+  }
+
+  .box__content {
+    margin: .5rem 0;
+  }
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -3,7 +3,59 @@
     <p-navbar />
 
     <div class="homepage__content">
-      <h1>Hompage</h1>
+
+      <p-card class="home__content__summary">
+          <template #header>
+              <ul class="home__content__summary__tab-links">
+                <li
+                  class="home__content__summary__tab-links__item"
+                  :class="{
+                    'home__content__summary__tab-links__item--active': activeTab === 'WorkingOn'
+                  }"
+                  @click="toggleActiveTab('WorkingOn')">
+                  Working on
+                </li>
+                <li class="home__content__summary__tab-links__item"
+                  :class="{
+                    'home__content__summary__tab-links__item--active': activeTab === 'Deadlines'
+                  }"
+                  @click="toggleActiveTab('Deadlines')">
+                  Deadlines
+                </li>
+              </ul>
+          </template>
+          <template #content>
+            <div class="home__content__summary__tabs">
+              <div v-show="activeTab === 'WorkingOn'" class="home__content__summary__tabs__tab">
+                <p-box
+                  class="home__content__summary__tabs__tab__item"
+                  v-for="task in tasks"
+                  :key="task.id">
+                  <template #header>
+                    {{ task.name }}
+                  </template>
+                  <template #content>
+                    {{ task.description }}
+                  </template>
+                </p-box>
+              </div>
+
+              <div v-show="activeTab === 'Deadlines'" class="home__content__summary__tabs__tab">
+                <p-box
+                  class="home__content__summary__tabs__tab__item"
+                  v-for="deadline in deadlines"
+                  :key="deadline.id">
+                  <template #header>
+                    {{ deadline.date }}
+                  </template>
+                  <template #content>
+                    {{ deadline.id }}
+                  </template>
+                </p-box>
+              </div>
+            </div>
+          </template>
+      </p-card>
 
       <ul>
         <h1>Projects</h1>
@@ -52,23 +104,33 @@
 
 <script>
 import PNavbar from '@/components/PNavbar.vue';
+import PCard from '@/components/PCard.vue';
+import PBox from '@/components/PBox.vue';
 
 export default {
   name: 'Homepage',
   components: {
     PNavbar,
+    PCard,
+    PBox,
   },
   data() {
     return {
       projects: null,
       deadlines: null,
       tasks: null,
+      activeTab: 'WorkingOn',
     };
   },
   mounted() {
     this.projects = this.$store.getters['projects/getProjects'];
     this.deadlines = this.$store.getters['deadlines/getDeadlines'];
     this.tasks = this.$store.getters['tasks/getTasks'];
+  },
+  methods: {
+    toggleActiveTab(activeTab) {
+      this.activeTab = activeTab;
+    },
   },
 };
 </script>
@@ -82,7 +144,41 @@ export default {
   grid-template-rows: 4rem auto;
 
   .homepage__content {
+    padding: 2rem .5rem;
+
     overflow-y: scroll;
+
+    .home__content__summary {
+      height: 70%;
+
+      overflow-y: hidden;
+
+      .home__content__summary__tab-links {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+
+        .home__content__summary__tab-links__item {
+          font-weight: $font-bold;
+          text-transform: uppercase;
+          color: $lighen-color;
+
+          list-style-type: none;
+
+          &.home__content__summary__tab-links__item--active {
+            color: $base-color;
+          }
+        }
+      }
+
+      .home__content__summary__tabs {
+        .home__content__summary__tabs__tab {
+          .home__content__summary__tabs__tab__item {
+            margin: 1rem 0;
+          }
+        }
+      }
+    }
   }
 }
 </style>
