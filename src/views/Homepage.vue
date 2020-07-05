d0ace700a57eabaf4eb6d630147c4b062e66b005
Andrea Longo <longoandrea.dev@gmail.com>
2020-07-05 19:12:59 +0200

    Added homepage structure

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,30 +1,52 @@
 <template>
-  <div>
+  <div class="homepage">
     <p-navbar />
-    <h1>Hompage</h1>
 
-    <ul>
-      <h1>Projects</h1>
-      <li v-for="project in projects" :key="project.id">
-        <router-link :to="`project/${project.id}`">{{ project.name }}</router-link>
-      </li>
-    </ul>
+    <div class="homepage__content">
+      <h1>Hompage</h1>
 
-    <ul>
-      <h1>Deadlines</h1>
-      <li v-for="deadline in deadlines" :key="deadline.id">
-        {{ deadline.date }}
-      </li>
-    </ul>
+      <ul>
+        <h1>Projects</h1>
+        <li v-for="project in projects" :key="project.id">
+          <router-link :to="`project/${project.id}`">{{ project.name }}</router-link>
+        </li>
+      </ul>
 
-    <ul>
-      <h1>Tasks</h1>
-      <li v-for="task in tasks" :key="task.id">
-        {{ task.name }}
-      </li>
-    </ul>
+      <ul>
+        <h1>Deadlines</h1>
+        <li v-for="deadline in deadlines" :key="deadline.id">
+          {{ deadline.date }}
+        </li>
+      </ul>
 
-    <router-link to="/addproject">Add a project</router-link>
+      <ul>
+        <h1>Tasks</h1>
+        <li v-for="task in tasks" :key="task.id">
+          {{ task.name }}
+        </li>
+      </ul>
+
+      <router-link to="/addproject">Add a project</router-link>
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum
+      inventore debitis et ipsum rerum ullam quam dolores eligendi aspernatur eaque neque
+       consectetur quidem cupiditate itaque, dolorem repellat illum incidunt?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum
+      inventore debitis et ipsum rerum ullam quam dolores eligendi aspernatur eaque neque
+       consectetur quidem cupiditate itaque, dolorem repellat illum incidunt?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum
+      inventore debitis et ipsum rerum ullam quam dolores eligendi aspernatur eaque neque
+       consectetur quidem cupiditate itaque, dolorem repellat illum incidunt?
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum
+      inventore debitis et ipsum rerum ullam quam dolores eligendi aspernatur eaque neque
+       consectetur quidem cupiditate itaque, dolorem repellat illum incidunt?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum
+      inventore debitis et ipsum rerum ullam quam dolores eligendi aspernatur eaque neque
+       consectetur quidem cupiditate itaque, dolorem repellat illum incidunt?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime harum
+      inventore debitis et ipsum rerum ullam quam dolores eligendi aspernatur eaque neque
+       consectetur quidem cupiditate itaque, dolorem repellat illum incidunt?
+    </div>
   </div>
 </template>
 
@@ -52,4 +74,15 @@ export default {
 </script>
 
 <style lang="scss">
+.homepage {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 4rem auto;
+
+  .homepage__content {
+    overflow-y: scroll;
+  }
+}
 </style>
